6ac901651367e993763c014d8a32b6dad9512f97
DerrickMits <derrickodiwuor@gmail.com>
2024-08-13 06:46:43 -0700

    a recursive function that queries the Reddit API and returns a list containing the titles of all hot articles

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,50 +1,30 @@
 #!/usr/bin/python3
-""" Flask Application """
-from models import storage
-from api.v1.views import app_views
-from os import environ
-from flask import Flask, render_template, make_response, jsonify
-from flask_cors import CORS
-from flasgger import Swagger
-from flasgger.utils import swag_from
-
-app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+"""
+THis script shall return a list containing the titles of all hot articles for
+a given subreddit
+"""
+import requests
 
 
-@app.teardown_appcontext
-def close_db(error):
-    """ Close Storage """
-    storage.close()
-
-
-@app.errorhandler(404)
-def not_found(error):
-    """ 404 Error
-    ---
-    responses:
-      404:
-        description: a resource was not found
+def recurse(subreddit, hot_list=[], compte=''):
     """
-    return make_response(jsonify({'error': "Not found"}), 404)
-
-
-app.config['SWAGGER'] = {
-    'title': 'AirBnB clone Restful API',
-    'uiversion': 3
-}
-
-Swagger(app)
-
-
-if __name__ == "__main__":
-    """ Main Function """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True, debug=True)
+    THis shall query the Reddit API and return a list containing the titles
+    of all hot articles
+    """
+    bz_url = 'https://www.reddit.com'
+    url = '{base}/r/{subreddit}/hot.json'.format(base=bz_url,
+                                                 subreddit=subreddit)
+    usr_agt = {'User-Agent': 'Python/requests'}
+    pay = {'after': compte, 'count': '100'}
+    respo = requests.get(url, headers=usr_agt, params=pay,
+                         allow_redirects=False)
+    if respo.status_code == 200:
+        respo = respo.json()
+        psts = respo.get('data').get('children')
+        for pst in psts:
+            hot_list.append(pst.get('data').get('title'))
+        compte = respo.get('data').get('after')
+        if compte is not None:
+            return recurse(subreddit, hot_list, compte)
+        return hot_list
+    return None
